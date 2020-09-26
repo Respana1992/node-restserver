dad25f756f4f1226395e89c1c727b72fe919149d
Ricardo <bladesingle@gmail.com>
2020-09-26 03:47:40 -0500

    cadena conexion

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URI;
+    urlDB = 'mongodb+srv://bladesingle:<Admin123*@$>@cluster0.nal3l.mongodb.net/cafe'; //process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
