9aecb74f580347862935a790d1924d8d4a21365f
Ricardo <bladesingle@gmail.com>
2020-09-27 03:35:20 -0500

    Login nomal seccion 10

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -2,10 +2,14 @@ const express = require('express');
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion');
 const app = express();
 
 
-app.get('/usuario', function(req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
+
+
+
     let desde = req.query.desde || 0;
     desde = Number(desde);
 
@@ -39,7 +43,7 @@ app.get('/usuario', function(req, res) {
 
 })
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], function(req, res) {
     //console.log(req.body);
     let body = req.body;
 
@@ -70,7 +74,7 @@ app.post('/usuario', function(req, res) {
 
 })
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], function(req, res) {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -93,7 +97,7 @@ app.put('/usuario/:id', function(req, res) {
     })
 });
 
-app.delete('/usuario:id', function(req, res) {
+app.delete('/usuario:id', [verificaToken, verificaAdmin_Role], function(req, res) {
     let id = req.params.id;
     console.log(req.params.id);
     // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => {
--- server/server.js
+++ server/server.js
@@ -16,8 +16,9 @@ app.use(bodyParser.urlencoded({
 // parse application/json
 app.use(bodyParser.json())
 
+//Configuracion global de rutas
+app.use(require('./routes/index'));
 
-app.use(require('./routes/usuario'));
 mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
     (err, res) => {
         if (err) throw err;
